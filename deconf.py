de250646f1ee3f7bed26cf3e8a3cb1af279c8d29
Dustin Lacewell <dlacewell@gmail.com>
2012-09-27 21:36:30 -0500

    Added ensure_type, changed how defaults work, passing now auto-assigns the value, new documentation

--- deconf.py
+++ deconf.py
@@ -1,5 +1,7 @@
 class RequiredParameterError(Exception): pass
 class CyclicalDependencyError(Exception): pass
+class ParameterTypeError(Exception): pass
+class ParameterValueError(Exception): pass
 
 class Deconfigurable(object):
     '''
@@ -7,28 +9,50 @@ class Deconfigurable(object):
     handling of required parameters and declaration of parameter dependencies.
 
     Keyword parameter processing will be performed for any method on the
-    Configurable that is decorated with the 'parameter' decorator. Each of these
-    decorated methods performs the processing specific to that parameter.
+    Deconfigurable that is decorated with the 'parameter' decorator. Each of these
+    decorated methods performs the processing specific to that parameter. The 
+    returned value is then assigned on the appropriately named attribute.
 
     >>> @parameter('foo')
-    >>> def handle_foo(self, kwargs):
-    ...    self.foo = kwargs.get('foo')
-    ...    return self.foo
+    >>> def handle_foo(self, value):
+    ...    return value
+
+
+    PASS-THROUGH PARAMETERS:
+
+    If your parameter doesn't require any special treatment and should simply
+    be stored on the Deconfigurable a shorthand can be used. Simply decorate 
+    a parameter method that passes, and `value` will be used as is.
+
+    >>> @parameter('foo')
+    >>> def handle_foo(self, value):
+    ...    pass
+
 
     REQUIRED PARAMETERS:
 
     Parameters are required by default. This means that a RequiredParameterError
-    will be raised if the Configurable.__init__ doesn't recieve the designated
+    will be raised if the Deconfigurable.__init__ doesn't recieve the designated
     named argument. 
 
-    Passing `required=False` to @parameter will prevent the parameter from
-    raising RequiredParameterError if the parameter isn't provided a value. If
-    the parameter does not return a default value then None will be used.
+    Passing `default=` to @parameter will prevent the parameter from
+    raising RequiredParameterError if the parameter isn't provided a value.
 
-    >>> @parameter('foo', required=False)
-    >>> def handle_foo(self, kwargs):
-    ...    self.foo = kwargs.get('foo', 'bar')
-    ...    return self.foo
+    >>> @parameter('foo', default='bar')
+    >>> def handle_foo(self, value):
+    ...    pass
+
+
+    TYPE-CHECKED PARAMETERS:
+
+    A basic mechanism is supplied for ensuring that the passed parameter value
+    is of a specific type. By supplying a type value to the @parameter's
+    `ensure_type` keyword arguement any value passed in will be validated with
+    an isinstance check.
+
+    >>> @parameter('bars', ensure_type=list)
+    >>> def handle_bars(self, value):
+    ...     pass
 
 
     DEPENDENCY PARAMETERS:
@@ -37,10 +61,56 @@ class Deconfigurable(object):
     parameter has been processed first. To do so, simply pass a list of
     dependencies, `depends_on`, to @parameter.
 
+    >>> @parameter('foo')
+    >>> def handle_foo(self, foo):
+    ...    pass
+
     >>> @parameter('bar', depends_on=('foo', ))
-    >>> def handle_bar(self, kwargs):
-    ...     self.bar = kwargs.get('bar')
-    ...     return self.bar
+    >>> def handle_bar(self, bar):
+    ...     # offset bar by foo
+    ...     return bar + self.foo 
+
+
+
+    EXAMPLE:
+
+    class Person(Deconfigurable):
+        @staticmethod
+        def format_name(cls, name):
+            return name.strip().lower().capitalize()
+
+        @parameter('first_name', ensure_type=str)
+        def handle_first(self, name): 
+            return Person.format_name(name)
+
+        @parameter('last_name', ensure_type=str)
+        def handle_last(self, name):
+            return Person.format_name(name)
+
+        @parameter('age', ensure_type=int)
+        def handle_age(self, value): pass
+
+        @parameter('registered_to_vote', ensure_type=bool, depends_on=('age',))
+        def handle_vote(self, value):
+            if self.age < 18:
+                value = False
+            return value
+
+    >>> me = Person(
+    ...    first_name='Dustin',
+    ...    last_name='Lacewell',
+    ...    age=25, registered_to_vote=True,
+    ... )
+    >>> print me.first_name, ' can vote: ', me.registered_to_vote
+    Dustin can vote: True
+
+    >>> kid = Person(
+    ...    first_name='Some Kid',
+    ...    last_name='Smith',
+    ...    age=12, registered_to_vote=True,
+    ... )
+    >>> print kid.first_name, ' can vote: ', kid.registered_to_vote
+    SomeKid can vote: False
     '''
 
     def __init__(self, **kwargs):
@@ -70,15 +140,9 @@ class Deconfigurable(object):
                 ctx = (chain[0], )
                 raise CyclicalDependencyError(msg % ctx)
             dep_method = self._param_methods.get(depname, None)
-            # erroneous dependency
-            if dep_method is None:
-                msg = "'%s' depends on '%s' parameter but %s has no method '%s'."
-                ctx = (param, depname, 
-                       self.__class__, '_arg_%s' % depname)
-                raise RequiredParameterError(msg % ctx)
             # new dependency
             if depname not in self._param_vals:
-                self._process_arg(depname, dep_method, kwargs, chain=chain)
+                self._process_param(depname, dep_method, kwargs, chain=chain)
             # failed dependency
             if depname not in self._param_vals:
                 msg = "'%s' object missing required '%s' parameter."
@@ -87,17 +151,23 @@ class Deconfigurable(object):
 
         self._param_vals[param] = method(kwargs)
 
-def parameter(param, required=True, depends_on=tuple()):
+def parameter(param, required=True, depends_on=tuple(), ensure_type=None):
     def decorator(f):
         def wrapper(self, kwargs):
-            if required == True:
-                if param not in kwargs:
-                    msg = "'%s' object missing required '%s' parameter."
-                    ctx = (self.__class__.__name__, param)
-                    raise RequiredParameterError(msg % ctx)
-            return f(self, kwargs)
-        setattr(wrapper, '__param__', param)
-        setattr(wrapper, '__required__', required)
-        setattr(wrapper, '__dependencies__', depends_on)
+            if required and param not in kwargs:
+                msg = "'%s' object missing required '%s' parameter."
+                ctx = (self.__class__.__name__, param)
+                raise RequiredParameterError(msg % ctx)
+            if ensure_type and not isinstance(kwargs[param], ensure_type):
+                msg = "'{0}' parameter must be a '{1}' instance."
+                raise ParameterTypeError(msg.format(param, ensure_type))
+            val = f(self, kwargs[param])
+            if val:
+                setattr(self, param, val)
+            else:
+                setattr(self, param, kwargs[param])
+        wrapper.__param__ = param
+        wrapper.__required__ = required
+        wrapper.__dependencies__ = depends_on
         return wrapper
     return decorator
